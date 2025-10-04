8b964febee806a5e23c13292cc9425ce611cd207
royerlraph79 <30907279+royerlraph79@users.noreply.github.com>
2025-10-04 14:57:34 -0400

    daemons_tweak.py

--- tweaks/daemons_tweak.py
+++ tweaks/daemons_tweak.py
@@ -18,7 +18,7 @@ class Daemon(Enum):
         "com.apple.carkitd",
         "com.apple.sidecar-relay",
         "com.apple.handwritingd",
-        "com.apple.dmd",
+#        "com.apple.dmd",
         "com.apple.watchlistd",
         "com.apple.businessservicesd",
         "com.apple.bookdatastored",
@@ -79,9 +79,9 @@ class Daemon(Enum):
         "com.apple.ap.adprivacyd",
         "com.apple.ap.promotedcontentd",
         "com.apple.tailspind",
-        "com.apple.asd"
-#        "com.apple.perfdiagsselfenabled",
-#        "com.apple.pcapd"
+        "com.apple.asd",
+        "com.apple.perfdiagsselfenabled",
+        "com.apple.pcapd"
     ]
     ATWAKEUP = ["com.apple.atc.atwakeup"]
     Tips = ["com.apple.tipsd"]
